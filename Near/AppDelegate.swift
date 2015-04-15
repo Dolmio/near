436ce9845fc2b426d09d18edfc9bd555fd25309e
SieniMaagi <laurilavanti@gmail.com>
2015-04-15 11:44:17 +0300

    Finished the city changing logic, needs to be tested on a real device though.

--- Near/AppDelegate.swift
+++ Near/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
             placeController!.setupPlacesAndRegions()
             NSUserDefaults.standardUserDefaults().setBool(true, forKey: "firstLaunch")
         }
+        //FIXME: this is for testing purposes, should use significant changes.
+        placeController?.locationManager.startMonitoringSignificantLocationChanges()
         return true
     }
 
